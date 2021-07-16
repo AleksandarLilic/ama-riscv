87348affb3b182ac05f16b7b24c1e1f7d56146a5
AleksandarLilic <aleksandarlilic03@gmail.com>
2021-07-16 08:27:26 +0200

    feat(defines): update defines 0.7.0

--- src/ama_riscv_defines.v
+++ src/ama_riscv_defines.v
@@ -11,10 +11,50 @@
 //      2021-07-11  AL  0.2.0 - Add Imm Gen defines
 //      2021-07-13  AL  0.3.0 - Add Imm Gen Disabled define, shift others
 //      2021-07-13  AL  0.4.0 - Add Register File defines
+//      2021-07-13  AL  0.5.0 - Add DMEM access defines
+//      2021-07-16  AL  0.6.0 - Add Opcode 5-bit defines
+//      2021-07-16  AL  0.7.0 - Add MUX select defines (PC, ALU_A op, ALU_B op, WB)
 //
 //-----------------------------------------------------------------------------
+// Opcodes
+// Only top 5 bits of a 7-bit opcode is needed
+// All RV32I instructions have format of {OPC5,2'b11}
+`define OPC5_ARI_R_TYPE  5'b0_1100      // R-type
+`define OPC5_ARI_I_TYPE  5'b0_0100      // I-type
+`define OPC5_LOAD        5'b0_0000      // I-type
+`define OPC5_STORE       5'b0_1000      // S-type
+`define OPC5_BRANCH      5'b1_1000      // B-type
+`define OPC5_JALR        5'b1_1001      // J-type, I-format
+`define OPC5_JAL         5'b1_1011      // J-type
+`define OPC5_LUI         5'b0_1101      // U-type
+`define OPC5_AUIPC       5'b0_0101      // U-type
+
+//-----------------------------------------------------------------------------
+// MUX select signals
+// PC select
+`define PC_SEL_INC4         2'd0  // PC = PC + 4
+`define PC_SEL_ALU          2'd1  // ALU output, used for jump/branch
+`define PC_SEL_BP           2'd2  // PC = Branch prediction output
+`define PC_SEL_START_ADDR   2'd3  // PC = Hardwired start address
+
+// ALU A operand select
+`define ALU_A_SEL_RS1       2'd0  // A = Reg[rs1]
+`define ALU_A_SEL_PC        2'd1  // A = PC
+`define ALU_A_SEL_FW_ALU    2'd2  // A = ALU; forwarding from MEM stage
+
+// ALU B operand select
+`define ALU_B_SEL_RS2       2'd0  // B = Reg[rs2]
+`define ALU_B_SEL_IMM       2'd1  // B = Immediate value; from Imm Gen
+`define ALU_B_SEL_FW_ALU    2'd2  // B = ALU; forwarding from MEM stage
+
+// Write back select
+`define WB_SEL_DMEM         2'd0  // Reg[rd] = DMEM[ALU]
+`define WB_SEL_ALU          2'd1  // Reg[rd] = ALU
+`define WB_SEL_INC4         2'd2  // Reg[rd] = PC + 4
+
+//-----------------------------------------------------------------------------
 // Register File
-`define RF_X0_ZERO  5'd0 // hard-wired zero
+`define RF_X0_ZERO  5'd0  // hard-wired zero
 `define RF_X1_RA    5'd1  // return address
 `define RF_X2_SP    5'd2  // stack pointer 
 `define RF_X3_GP    5'd3  // global pointer
@@ -47,6 +87,8 @@
 `define RF_X30_T5   5'd30 // temporary
 `define RF_X31_T6   5'd31 // temporary
 
+//-----------------------------------------------------------------------------
+// DMEM access
 // DMEM Width
 `define DMEM_BYTE   2'd0
 `define DMEM_HALF   2'd1
@@ -58,6 +100,7 @@
 `define DMEM_OFF_2  2'd2
 `define DMEM_OFF_3  2'd3
 
+//-----------------------------------------------------------------------------
 // ALU
 `define ALU_ADD     4'b0000
 `define ALU_SUB     4'b1000
@@ -71,6 +114,7 @@
 `define ALU_AND     4'b0111
 `define ALU_PASS_B  4'b1111
 
+//-----------------------------------------------------------------------------
 // Imm Gen
 `define IG_DISABLED 3'b000
 `define IG_I_TYPE   3'b001
