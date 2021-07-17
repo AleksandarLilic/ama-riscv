58ea718ef78f06100b7f1fb6d54827b738bbf4bb
AleksandarLilic <aleksandarlilic03@gmail.com>
2021-07-17 09:09:58 +0200

    test(decoder): update decoder_tb 0.2.0 - add file read, finish r-type

--- verif/direct_tb/ama_riscv_decoder_tb.v
+++ verif/direct_tb/ama_riscv_decoder_tb.v
@@ -10,6 +10,7 @@
 //
 // Version history:
 //      2021-07-16  AL  0.1.0 - Initial - Reset and R-types (Add & Sub only)
+//      2021-07-17  AL  0.2.0 - Add file reads, finish R-types
 //-----------------------------------------------------------------------------
 
 `timescale 1ns/1ps
@@ -18,6 +19,7 @@
 //`define CLOCK_FREQ    125_000_000
 //`define SIM_TIME     `CLOCK_FREQ*0.0009 // 900us
 `define TEST_CASES              64
+`define R_TYPE_TESTS            11
 
 // MUX select signals
 // PC select
@@ -40,6 +42,8 @@
 `define WB_SEL_DMEM         2'd0  // Reg[rd] = DMEM[ALU]
 `define WB_SEL_ALU          2'd1  // Reg[rd] = ALU
 `define WB_SEL_INC4         2'd2  // Reg[rd] = PC + 4
+
+`define PROJECT_PATH        "C:/Users/Aleksandar/Documents/xilinx/ama-riscv/"
 
 module ama_riscv_decoder_tb();
 
@@ -96,14 +100,16 @@ reg  [ 1:0] dut_m_wb_sel     ;
 reg         dut_m_reg_we     ;
 
 // Testbench variables
-//reg         done;
 integer     i;              // used for all loops
 integer     inst_ii = 0;    // used for instruction array access only
 integer     errors;
-reg  [31:0] test_values_inst   [`TEST_CASES-1:0];
-// reg  [ 4:0] test_values_op_sel [`TEST_CASES-1:0];
-// reg  [31:0] test_values_in_b   [`TEST_CASES-1:0];
-// reg  [ 3:0] received_values    [`TEST_CASES-1:0];
+// file read
+integer fd;
+integer status;
+reg  [20*7:0] str;
+reg  [  31:0] test_values_inst_hex [`TEST_CASES-1:0];
+reg  [20*7:0] test_values_inst_asm [`TEST_CASES-1:0];
+// events
 event ev_rst    [1:0];
 event ev_decode [1:0];
 
@@ -157,111 +163,138 @@ task task_checker;
     begin    
         // pc_sel
         if (pc_sel != dut_m_pc_sel) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT pc_sel: 'b%2b, Model pc_sel: 'b%2b,", $time, test_values_inst[inst_ii], inst_ii, pc_sel, dut_m_pc_sel);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT pc_sel: 'b%2b, Model pc_sel: 'b%2b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], pc_sel, dut_m_pc_sel);
             errors = errors + 1;
         end
         
         // pc_we
         if (pc_we != dut_m_pc_we) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT pc_we: 'b%2b, Model pc_we: 'b%2b,", $time, test_values_inst[inst_ii], inst_ii, pc_we, dut_m_pc_we);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT pc_we: 'b%2b, Model pc_we: 'b%2b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], pc_we, dut_m_pc_we);
             errors = errors + 1;
         end
         
         // imem_en
         if (imem_en != dut_m_imem_en) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT imem_en: 'b%1b, Model imem_en: 'b%1b,", $time, test_values_inst[inst_ii], inst_ii, imem_en, dut_m_imem_en);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT imem_en: 'b%1b, Model imem_en: 'b%1b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], imem_en, dut_m_imem_en);
             errors = errors + 1;
         end
         
         // branch_inst
         if (branch_inst != dut_m_branch_inst) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT branch_inst: 'b%1b, Model branch_inst: 'b%1b,", $time, test_values_inst[inst_ii], inst_ii, branch_inst, dut_m_branch_inst);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT branch_inst: 'b%1b, Model branch_inst: 'b%1b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], branch_inst, dut_m_branch_inst);
             errors = errors + 1;
         end
         
         // store_inst
         if (store_inst != dut_m_store_inst) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT store_inst: 'b%1b, Model store_inst: 'b%1b,", $time, test_values_inst[inst_ii], inst_ii, store_inst, dut_m_store_inst);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT store_inst: 'b%1b, Model store_inst: 'b%1b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], store_inst, dut_m_store_inst);
             errors = errors + 1;
         end
         
         // alu_op_sel
         if (alu_op_sel != dut_m_alu_op_sel) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT alu_op_sel: 'b%4b, Model alu_op_sel: 'b%4b,", $time, test_values_inst[inst_ii], inst_ii, alu_op_sel, dut_m_alu_op_sel);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT alu_op_sel: 'b%4b, Model alu_op_sel: 'b%4b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], alu_op_sel, dut_m_alu_op_sel);
             errors = errors + 1;
         end
         
         // alu_a_sel
         if (alu_a_sel != dut_m_alu_a_sel) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT alu_a_sel: 'b%1b, Model alu_a_sel: 'b%1b,", $time, test_values_inst[inst_ii], inst_ii, alu_a_sel, dut_m_alu_a_sel);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT alu_a_sel: 'b%1b, Model alu_a_sel: 'b%1b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], alu_a_sel, dut_m_alu_a_sel);
             errors = errors + 1;
         end
         
         // alu_b_sel
         if (alu_b_sel != dut_m_alu_b_sel) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT alu_b_sel: 'b%1b, Model alu_b_sel: 'b%1b,", $time, test_values_inst[inst_ii], inst_ii, alu_b_sel, dut_m_alu_b_sel);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT alu_b_sel: 'b%1b, Model alu_b_sel: 'b%1b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], alu_b_sel, dut_m_alu_b_sel);
             errors = errors + 1;
         end
         
         // ig_sel
         if (ig_sel != dut_m_ig_sel) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT ig_sel: 'b%3b, Model ig_sel: 'b%3b,", $time, test_values_inst[inst_ii], inst_ii, ig_sel, dut_m_ig_sel);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT ig_sel: 'b%3b, Model ig_sel: 'b%3b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], ig_sel, dut_m_ig_sel);
             errors = errors + 1;
         end
         
         // bc_uns
         if (bc_uns != dut_m_bc_uns) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT bc_uns: 'b%1b, Model bc_uns: 'b%1b,", $time, test_values_inst[inst_ii], inst_ii, bc_uns, dut_m_bc_uns);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT bc_uns: 'b%1b, Model bc_uns: 'b%1b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], bc_uns, dut_m_bc_uns);
             errors = errors + 1;
         end
         
         // dmem_en
         if (dmem_en != dut_m_dmem_en) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT dmem_en: 'b%1b, Model dmem_en: 'b%1b,", $time, test_values_inst[inst_ii], inst_ii, dmem_en, dut_m_dmem_en);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT dmem_en: 'b%1b, Model dmem_en: 'b%1b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], dmem_en, dut_m_dmem_en);
             errors = errors + 1;
         end
         
         // load_sm_en
         if (load_sm_en != dut_m_load_sm_en) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT load_sm_en: 'b%1b, Model load_sm_en: 'b%1b,", $time, test_values_inst[inst_ii], inst_ii, load_sm_en, dut_m_load_sm_en);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT load_sm_en: 'b%1b, Model load_sm_en: 'b%1b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], load_sm_en, dut_m_load_sm_en);
             errors = errors + 1;
         end
         
         // wb_sel
         if (wb_sel != dut_m_wb_sel) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT wb_sel: 'b%2b, Model wb_sel: 'b%2b,", $time, test_values_inst[inst_ii], inst_ii, wb_sel, dut_m_wb_sel);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT wb_sel: 'b%2b, Model wb_sel: 'b%2b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], wb_sel, dut_m_wb_sel);
             errors = errors + 1;
         end
         
         // reg_we
         if (reg_we != dut_m_reg_we) begin
-            $display("*ERROR @ %0t. Input inst: 'h%8h (#%0d), DUT reg_we: 'b%1b, Model reg_we: 'b%1b,", $time, test_values_inst[inst_ii], inst_ii, reg_we, dut_m_reg_we);
+            $display("*ERROR @ %0t. Input inst: 'h%8h  %0s, DUT reg_we: 'b%1b, Model reg_we: 'b%1b,", 
+            $time, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii], reg_we, dut_m_reg_we);
             errors = errors + 1;
         end
     
     end // main task body */
 endtask
 
-task generate_test_array_values;
+task read_test_instructions;
     begin
-        // non random part:
-        test_values_inst[0] = 'h003100b3;   // add  x1 x2 x3
-        test_values_inst[1] = 'h403100b3;   // sub  x1 x2 x3
-        test_values_inst[2] = 'h003110b3;   // sll  x1 x2 x3
-        test_values_inst[3] = 'h003120b3;   // slt  x1 x2 x3
-        test_values_inst[4] = 'h003130b3;   // sltu x1 x2 x3
-        test_values_inst[5] = 'h003140b3;   // xor  x1 x2 x3
-        test_values_inst[6] = 'h003150b3;   // srl  x1 x2 x3
-        test_values_inst[7] = 'h403150b3;   // sra  x1 x2 x3
-        test_values_inst[8] = 'h003160b3;   // or   x1 x2 x3
-        test_values_inst[9] = 'h003170b3;   // and  x1 x2 x3
+        // Instructions HEX
+        fd = $fopen({`PROJECT_PATH, "verif/direct_tb/inst/decoder_inst_hex.txt"}, "r");
+    
+        if (fd == 0) begin
+            $display("fd handle was NULL");        
+        end
+    
+        i = 0;
+        while(!$feof(fd)) begin
+            $fscanf (fd, "%h", test_values_inst_hex[i]);
+            // $display("'h%h", test_values_inst_hex[i]);
+            i = i + 1;
+        end
+        $fclose(fd);
         
-        /* // random part:
-        for (i = 0; i < `TEST_CASES; i = i + 1) begin
-           test_values_op_sel[i]  <= $random;
-           test_values_inst  [i]  <= $random;
-           test_values_in_b  [i]  <= $random;
-         end */
+        // Instructions ASM
+        fd = $fopen({`PROJECT_PATH, "verif/direct_tb/inst/decoder_inst_asm.txt"}, "r");
+        
+        if (fd == 0) begin
+            $display("fd handle was NULL");        
+        end
+                
+        i = 0;
+        while(!$feof(fd)) begin
+            status = $fgets(str, fd);
+            // $write("%0s", str);
+            test_values_inst_asm[i] = str;
+            // $write("%0s", test_values_inst_asm[i]);
+            i = i + 1;
+        end
+        $fclose(fd);
     end
 endtask
 
@@ -325,7 +358,6 @@ task reset;
             ->ev_rst[1];            
         end
         rst = 1'b0;
-        // @(posedge clk); #1;
     end
     
 endtask
@@ -335,9 +367,10 @@ endtask
 initial begin
     @(ev_rst[0]);
     dut_m_task_reset();
+    
     forever begin
         @(ev_decode[0])
-        dut_m_task_decode(test_values_inst[inst_ii]);
+        dut_m_task_decode(test_values_inst_hex[inst_ii]);
     end
     
 end
@@ -347,6 +380,7 @@ end
 initial begin
     @(ev_rst[1]);
     task_checker();
+    
     forever begin
         @(ev_decode[1])
         task_checker();
@@ -358,59 +392,35 @@ end
 // Config
 initial begin
     //Prints %t scaled in ns (-9), with 2 precision digits, with the " ns" string
-    $timeformat(-9, 2, " ns", 20);
-    
-    generate_test_array_values();
-    
-    //done   <= 0;
+    $timeformat(-9, 2, " ns", 20);    
+    read_test_instructions();
     errors <= 0;
 end
 
 //-----------------------------------------------------------------------------
 // Test
 initial begin
-    $display("\n------------------------ Testing started -------------------------\n\n");
+    $display("\n----------------------- Simulation started -----------------------\n\n");
     reset(4'd2);
     
     //-----------------------------------------------------------------------------
     // Test 1: Hit specific cases
-    $display("Test  1: Hit specific cases ...");
+    $display("Test  1: Hit specific cases R-type ...\n");
     
-    // $display("Run 1: add  x1 x2 x3");
-    task_driver(test_values_inst[inst_ii]);
-    @(posedge clk); #1;
-    ->ev_decode[0];
-    ->ev_decode[1];
-    #1;
+    repeat(`R_TYPE_TESTS) begin
+        task_driver(test_values_inst_hex[inst_ii]);
+        @(posedge clk); #1;
+        ->ev_decode[0];
+        ->ev_decode[1];
+        #1;
+        $write("Run %2d done. Instruction: 'h%8h   %0s", 
+        inst_ii, test_values_inst_hex[inst_ii], test_values_inst_asm[inst_ii]);
+        inst_ii = inst_ii + 1;
+    end
     
-    // $display("Run 2: sub  x1 x2 x3");
-    inst_ii = inst_ii + 1;
-    task_driver(test_values_inst[inst_ii]);
-    @(posedge clk); #1;
-    ->ev_decode[0];
-    ->ev_decode[1];
-    #1;
+    $display("\nTest  1: Checking specific cases R-type done\n");
     
-    /* 
-    // $display("Run 3: op: 4'b1111 (PASS_B)");
-    check(4'b1111, 32'd35, 32'd192);
-    #1;
-    
-    // $display("Run 4: op: 4'b1101 (SRA)");
-    check(4'b1101, 32'd35, 32'd4);
-    #1;
-    
-    // $display("Run 5: op: 4'b0101 (SRL)");
-    check(4'b0101, 32'd35, 32'd4);
-    #1;
-    
-    // $display("Run 6: op: 4'b0001 (SLL)");
-    check(4'b0001, 32'd35, 32'd4);
-    #1;
-    
-    $display("Test  1: Checking specific cases done\n");
-    @(posedge clk); #1;
-    
+    /*    
     //-----------------------------------------------------------------------------
     // Test 2: Random hits (incl. invalid operations)
     $display("Test  2: Random hits ...");
@@ -434,7 +444,7 @@ initial begin
     else
         $display("Failed");
     $display("Errors: %0d", errors);
-    $display("----------------- End of the simulation results ------------------\n");
+    $display("--------------------- End of the simulation ----------------------\n");
     $finish();
 end
 
