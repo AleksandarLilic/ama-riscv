839499e3a15f0a3765c17aedb062a7df34f0cc7a
AleksandarLilic <aleksandarlilic03@gmail.com>
2021-08-16 06:57:20 +0200

    feat(operand_forwarding): update operand_forwarding 0.2.0 - add dmem data input forwarding

--- src/ama_riscv_operand_forwarding.v
+++ src/ama_riscv_operand_forwarding.v
@@ -8,21 +8,26 @@
 //
 // Version history:
 //      2021-08-12  AL  0.1.0 - Initial - Add forwarding
+//      2021-08-16  AL  0.2.0 - Add DMEM data input forwarding
+//                            - Change select signal names
+//                            - Add b_op condition to avoid forwarding rs2 for store
 //
 //-----------------------------------------------------------------------------
 `include "ama_riscv_defines.v"
 
 module ama_riscv_operand_forwarding (
     // inputs                         
-    input   wire        reg_we_ex     ,
-    input   wire [ 5:0] rs1_id        ,
-    input   wire [ 5:0] rs2_id        ,
-    input   wire [ 5:0] rd_ex         ,
-    input   wire        alu_a_sel     ,
-    input   wire        alu_b_sel     ,
-    // outputs
-    output  reg  [ 1:0] alu_a_sel_fwd ,
-    output  reg  [ 1:0] alu_b_sel_fwd
+    input   wire        reg_we_ex         ,
+    input   wire        store_inst_id     ,
+    input   wire [ 5:0] rs1_id            ,
+    input   wire [ 5:0] rs2_id            ,
+    input   wire [ 5:0] rd_ex             ,
+    input   wire        a_op_sel          ,
+    input   wire        b_op_sel          ,
+    // outputs                            
+    output  reg  [ 1:0] a_op_sel_fwd      ,
+    output  reg  [ 1:0] b_op_sel_fwd      ,
+    output  wire        dmem_din_sel_fwd
 );
 
 //-----------------------------------------------------------------------------
@@ -32,19 +37,22 @@ module ama_riscv_operand_forwarding (
 // A operand select
 always @ (*) begin
     if ((rs1_id != `RF_X0_ZERO) && (rs1_id == rd_ex) && (reg_we_ex))
-        alu_a_sel_fwd = `ALU_A_SEL_FWD_ALU;     // forward previous ALU result
+        a_op_sel_fwd = `ALU_A_SEL_FWD_ALU;      // forward previous ALU result
     else
-        alu_a_sel_fwd = {1'b0, alu_a_sel};      // don't forward
+        a_op_sel_fwd = {1'b0, a_op_sel};        // don't forward
 end
 
 //-----------------------------------------------------------------------------
 // B operand select
 always @ (*) begin
-    if ((rs2_id != `RF_X0_ZERO) && (rs2_id == rd_ex) && (reg_we_ex))
-        alu_b_sel_fwd = `ALU_B_SEL_FWD_ALU;     // forward previous ALU result
+    if ((rs2_id != `RF_X0_ZERO) && (rs2_id == rd_ex) && (reg_we_ex) && (!store_inst_id))
+        b_op_sel_fwd = `ALU_B_SEL_FWD_ALU;      // forward previous ALU result
     else
-        alu_b_sel_fwd = {1'b0, alu_b_sel};      // don't forward
+        b_op_sel_fwd = {1'b0, b_op_sel};        // don't forward
 end
 
+//-----------------------------------------------------------------------------
+// Store DMEM data input forward
+assign dmem_din_sel_fwd = (rs2_id != `RF_X0_ZERO) && (rs2_id == rd_ex) && (reg_we_ex) && (store_inst_id);
 
 endmodule
