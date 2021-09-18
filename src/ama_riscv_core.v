d710e419aaa9775a276b6f37846459051ae4ec3a
AleksandarLilic <aleksandarlilic03@gmail.com>
2021-09-18 09:05:18 +0200

    feat(core): core 0.3.0 - finish id/ex pipeline

--- src/ama_riscv_core.v
+++ src/ama_riscv_core.v
@@ -10,6 +10,7 @@
 //      2021-09-11  AL  0.1.0 - Initial - IF stage
 //      2021-09-13  AL  0.1.1 - Fix IMEM address signal
 //      2021-09-14  AL  0.2.0 - Add ID stage
+//      2021-09-18  AL  0.3.0 - Finish ID/EX pipeline
 //
 //-----------------------------------------------------------------------------
 `include "ama_riscv_defines.v"
@@ -23,46 +24,47 @@ module ama_riscv_core (
 // Signals
 
 // Pipeline control inputs
-wire        stall_if            ;
-wire        clear_if            ;
-wire        clear_id            ;
-wire        clear_ex            ;
-wire        clear_mem           ;
-
-// Signals - MEM stage
-wire        load_sm_en          ;
-wire [ 1:0] wb_sel              ;
-
-// Signals - EX stage
-reg  [31:0] inst_ex             ;
-reg         reg_we_ex           ;
-reg  [ 4:0] rd_addr_ex          ;
-reg         store_inst_ex       ;
-wire [ 3:0] alu_op_sel          ;
-wire [ 1:0] alu_a_sel_fwd       ;
-wire [ 1:0] alu_b_sel_fwd       ;
-wire        bc_a_sel_fwd        ;
-wire        bcs_b_sel_fwd       ;
-wire        bc_uns              ;
-wire        bc_a_eq_b           ;
-wire        bc_a_lt_b           ;
-wire [ 1:0] store_mask_offset   ;
-wire [ 3:0] dmem_we             ;
-wire        dmem_en             ;
+wire        stall_if                ;
+wire        clear_if                ;
+wire        clear_id                ;
+wire        clear_ex                ;
+wire        clear_mem               ;
+    
+// Signals - MEM stage  
+    
+// Signals - EX stage   
+reg  [31:0] inst_ex                 ;
+reg         reg_we_ex               ;
+reg  [ 4:0] rd_addr_ex              ;
+reg         store_inst_ex           ;
+// from datapath
+wire        bc_a_eq_b               ;
+wire        bc_a_lt_b               ;
+wire [ 1:0] store_mask_offset       ;
 
 // Signals - ID stage
-wire [31:0] inst_id             ;
-// wire        bp_taken  ;
-// wire        bp_clear  ;
-wire        store_inst_id       ;
-wire        branch_inst_id      ;
-wire        jump_inst_id        ;
-wire [ 2:0] imm_gen_sel         ;
-wire        reg_we_id           ;
+wire [31:0] inst_id                 ;
+// wire        bp_taken               ;
+// wire        bp_clear               ;
+wire        store_inst_id           ;
+wire        branch_inst_id          ;
+wire        jump_inst_id            ;
+wire [ 2:0] imm_gen_sel_id          ;
+wire [ 3:0] alu_op_sel_id           ;
+wire [ 1:0] alu_a_sel_fwd_id        ;
+wire [ 1:0] alu_b_sel_fwd_id        ;
+wire        bc_a_sel_fwd_id         ;
+wire        bcs_b_sel_fwd_id        ;
+wire        bc_uns_id               ;
+wire [ 3:0] dmem_we_id              ;
+wire        dmem_en_id              ;
+wire        load_sm_en_id           ;
+wire [ 1:0] wb_sel_id               ;
+wire        reg_we_id               ;
 
 // Signals - IF stage            
-wire [ 1:0] pc_sel              ;
-wire        pc_we               ;
+wire [ 1:0] pc_sel_if               ;
+wire        pc_we_if                ;
 
 //-----------------------------------------------------------------------------
 // Control
@@ -90,24 +92,24 @@ ama_riscv_control ama_riscv_control_i (
     // pipeline resets                  
                                         
     // outputs                          
-    .pc_sel             (pc_sel         ),
-    .pc_we              (pc_we          ),
-    // .imem_en            (imem_en        ),
-    .store_inst         (store_inst_id  ),
-    .branch_inst        (branch_inst_id ),
-    .jump_inst          (jump_inst_id   ),
-    .alu_op_sel         (alu_op_sel     ),
-    .ig_sel             (imm_gen_sel    ),
-    .bc_uns             (bc_uns         ),
-    .dmem_en            (dmem_en        ),
-    .load_sm_en         (load_sm_en     ),
-    .wb_sel             (wb_sel         ),
-    .reg_we             (reg_we_id      ),
-    .alu_a_sel_fwd      (alu_a_sel_fwd  ),
-    .alu_b_sel_fwd      (alu_b_sel_fwd  ),
-    .bc_a_sel_fwd       (bc_a_sel_fwd   ),
-    .bcs_b_sel_fwd      (bcs_b_sel_fwd  ),
-    .dmem_we            (dmem_we        )
+    .pc_sel             (pc_sel_if          ),
+    .pc_we              (pc_we_if           ),
+    // .imem_en            (imem_en           ),
+    .store_inst         (store_inst_id      ),
+    .branch_inst        (branch_inst_id     ),
+    .jump_inst          (jump_inst_id       ),
+    .alu_op_sel         (alu_op_sel_id      ),
+    .ig_sel             (imm_gen_sel_id     ),
+    .bc_uns             (bc_uns_id          ),
+    .dmem_en            (dmem_en_id         ),
+    .load_sm_en         (load_sm_en_id      ),
+    .wb_sel             (wb_sel_id          ),
+    .reg_we             (reg_we_id          ),
+    .alu_a_sel_fwd      (alu_a_sel_fwd_id   ),
+    .alu_b_sel_fwd      (alu_b_sel_fwd_id   ),
+    .bc_a_sel_fwd       (bc_a_sel_fwd_id    ),
+    .bcs_b_sel_fwd      (bcs_b_sel_fwd_id   ),
+    .dmem_we            (dmem_we_id         )
 );
 
 //-----------------------------------------------------------------------------
@@ -120,7 +122,7 @@ wire [31:0] alu_out     ;
 
 // PC select mux
 always @ (*) begin
-    case (pc_sel)
+    case (pc_sel_if)
         `PC_SEL_INC4:
             pc_mux_out =  pc_inc4;
         `PC_SEL_ALU:
@@ -138,7 +140,7 @@ end
 always @ (posedge clk) begin
     if (rst)
         pc <= 32'h0;
-    else if (pc_we)
+    else if (pc_we_if)
         pc <= pc_mux_out;
 end
 
@@ -180,14 +182,14 @@ reg  [ 4:0] rd_addr_mem ;
 // Signals - ID stage
 wire [31:0] pc_id = pc;
 // Reg file
-wire [ 4:0] rs1_addr   = inst_id[19:15];
-wire [ 4:0] rs2_addr   = inst_id[24:20];
-wire [ 4:0] rd_addr_id = inst_id[11: 7];
-wire [31:0] rd_data  = writeback;
+wire [ 4:0] rs1_addr    = inst_id[19:15];
+wire [ 4:0] rs2_addr    = inst_id[24:20];
+wire [ 4:0] rd_addr_id  = inst_id[11: 7];
+wire [31:0] rd_data     = writeback;
 wire [31:0] rs1_data_id ;
 wire [31:0] rs2_data_id ;
 // Imm Gen
-wire [24:0] imm_gen_in = inst_id[31: 7];
+wire [24:0] imm_gen_in  = inst_id[31: 7];
 wire [31:0] imm_gen_out_id  ;
 
 // Register File
@@ -213,51 +215,101 @@ ama_riscv_imm_gen ama_riscv_imm_gen_i(
    .clk     (clk            ),
    .rst     (rst            ),
    // inputs    
-   .ig_sel  (imm_gen_sel    ),
+   .ig_sel  (imm_gen_sel_id ),
    .ig_in   (imm_gen_in     ),
    // outputs
    .ig_out  (imm_gen_out_id )
 );
 
-// Pipeline FF
+//-----------------------------------------------------------------------------
+// Pipeline FF ID/EX
 // Signals
-reg  [31:0] pc_ex           ; 
-// reg  [ 4:0] rd_addr_ex      ;   // defined previously
-// reg         reg_we_ex       ;   // defined previously
-reg  [31:0] rs1_data_ex     ;
-reg  [31:0] rs2_data_ex     ;
-reg  [31:0] imm_gen_out_ex  ;
-// reg  [31:0] inst_ex         ;   // defined previously
+reg  [31:0] pc_ex               ; 
+// reg  [ 4:0] rd_addr_ex          ;   // defined previously
+reg  [31:0] rs1_data_ex         ;
+reg  [31:0] rs2_data_ex         ;
+reg  [31:0] imm_gen_out_ex      ;
+// reg  [31:0] inst_ex             ;   // defined previously
+reg         bc_a_sel_fwd_ex     ;
+reg         bcs_b_sel_fwd_ex    ;
+reg         bc_uns_ex           ;
+reg  [ 1:0] alu_a_sel_fwd_ex    ;
+reg  [ 1:0] alu_b_sel_fwd_ex    ;
+reg  [ 3:0] alu_op_sel_ex       ;
+reg         dmem_en_ex          ;
+reg  [ 3:0] dmem_we_ex          ;
+reg         load_sm_en_ex       ;
+reg  [ 1:0] wb_sel_ex           ;
+// reg         reg_we_ex           ;   // defined previously
 
 always @ (posedge clk) begin
     if (rst) begin
-        pc_ex           <= 32'h0;
-        rd_addr_ex      <=  5'h0;
-        reg_we_ex       <=  1'b0;
-        rs1_data_ex     <= 32'h0;
-        rs2_data_ex     <= 32'h0;
-        imm_gen_out_ex  <= 32'h0;
-        inst_ex         <= 32'h0;
+        // datapath
+        pc_ex            <= 32'h0;
+        rd_addr_ex       <=  5'h0;
+        rs1_data_ex      <= 32'h0;
+        rs2_data_ex      <= 32'h0;
+        imm_gen_out_ex   <= 32'h0;
+        inst_ex          <= 32'h0;
+        // control       
+        bc_a_sel_fwd_ex  <=  1'b0;
+        bcs_b_sel_fwd_ex <=  1'b0;
+        bc_uns_ex        <=  1'b0;
+        alu_a_sel_fwd_ex <=  2'h0;
+        alu_b_sel_fwd_ex <=  2'h0;
+        alu_op_sel_ex    <=  4'h0;
+        dmem_en_ex       <=  1'b0;
+        dmem_we_ex       <=  4'h0;
+        load_sm_en_ex    <=  1'b0;
+        wb_sel_ex        <=  2'h0;
+        reg_we_ex        <=  1'b0;
     end
     else if (clear_id) begin
-        pc_ex           <= 32'h0;
-        rd_addr_ex      <=  5'h0;
-        reg_we_ex       <=  1'b0;
-        rs1_data_ex     <= 32'h0;
-        rs2_data_ex     <= 32'h0;
-        imm_gen_out_ex  <= 32'h0;
-        inst_ex         <= 32'h0;
+        // datapath
+        pc_ex            <= 32'h0;
+        rd_addr_ex       <=  5'h0;
+        rs1_data_ex      <= 32'h0;
+        rs2_data_ex      <= 32'h0;
+        imm_gen_out_ex   <= 32'h0;
+        inst_ex          <= 32'h0;
+        // control       
+        bc_a_sel_fwd_ex  <=  1'b0;
+        bcs_b_sel_fwd_ex <=  1'b0;
+        bc_uns_ex        <=  1'b0;
+        alu_a_sel_fwd_ex <=  2'h0;
+        alu_b_sel_fwd_ex <=  2'h0;
+        alu_op_sel_ex    <=  4'h0;
+        dmem_en_ex       <=  1'b0;
+        dmem_we_ex       <=  4'h0;
+        load_sm_en_ex    <=  1'b0;
+        wb_sel_ex        <=  2'h0;
+        reg_we_ex        <=  1'b0;
     end
     else begin
-        pc_ex           <= pc_id            ;
-        rd_addr_ex      <= rd_addr_id       ;
-        reg_we_ex       <= reg_we_id        ;
-        rs1_data_ex     <= rs1_data_id      ;
-        rs2_data_ex     <= rs2_data_id      ;
-        imm_gen_out_ex  <= imm_gen_out_id   ;
-        inst_ex         <= inst_id          ;        
+        // datapath
+        pc_ex            <= pc_id           ;
+        rd_addr_ex       <= rd_addr_id      ;
+        rs1_data_ex      <= rs1_data_id     ;
+        rs2_data_ex      <= rs2_data_id     ;
+        imm_gen_out_ex   <= imm_gen_out_id  ;
+        inst_ex          <= inst_id         ;        
+        // control
+        bc_a_sel_fwd_ex  <= bc_a_sel_fwd_id ;
+        bcs_b_sel_fwd_ex <= bcs_b_sel_fwd_id;
+        bc_uns_ex        <= bc_uns_id       ;
+        alu_a_sel_fwd_ex <= alu_a_sel_fwd_id;
+        alu_b_sel_fwd_ex <= alu_b_sel_fwd_id;
+        alu_op_sel_ex    <= alu_op_sel_id   ;
+        dmem_en_ex       <= dmem_en_id      ;
+        dmem_we_ex       <= dmem_we_id      ;
+        load_sm_en_ex    <= load_sm_en_id   ;
+        wb_sel_ex        <= wb_sel_id       ;
+        reg_we_ex        <= reg_we_id       ;
     end
-end
+end      
+
+//-----------------------------------------------------------------------------
+// EX stage
 
 
 
