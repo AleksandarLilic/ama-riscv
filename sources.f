093ebb66dfc1cfde480d9ba6bc0ef5b59c9d56a3
Aleksandar <aleksandarlilic03@gmail.com>
2025-12-24 07:20:50 +0100

    core(simd): fix filelist change missing from 548f4b4

--- sources.f
+++ sources.f
@@ -14,7 +14,7 @@ sv work \
 "$REPO_ROOT/src/ama_riscv_reg_file.sv" \
 "$REPO_ROOT/src/ama_riscv_alu.sv" \
 "$REPO_ROOT/src/ama_riscv_simd.sv" \
-"$REPO_ROOT/src/ama_riscv_unpk.sv" \
+"$REPO_ROOT/src/ama_riscv_data_fmt.sv" \
 "$REPO_ROOT/src/csa.sv" \
 "$REPO_ROOT/src/csa_tree_8.sv" \
 "$REPO_ROOT/src/ama_riscv_icache.sv" \
